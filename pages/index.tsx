ada84371da72cc4599c8e6da7d5f79f2c6f00bfd
Hakob <hakob.andreasyan@popupschool.com>
2021-12-17 18:04:07 +0400

    project init

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  // next lesson _ 05 => 01
   return (
     <div className={styles.container}>
       <Head>
