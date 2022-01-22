7c2831bfdd49060f4e679d163142af0a0378c5a8
handreasyan <hakob.andreasyan@popupschool.com>
2022-01-23 00:30:58 +0400

    ssr

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,39 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export interface ITopPageAdvantage {
+  _id: string
+  title: string
+  description: string
+}
+
+export interface IHhData {
+  _id: string
+  count: string
+  juniorSalary: number
+  middleSalary: number
+  seniorSalary: number
+  updatedAt: Date
+}
+
+export interface ITopPageModel {
+  _id: string
+  tags: string[]
+  secondCategory: string
+  alias: string
+  title: string
+  category: string
+  seoText: string
+  tagsTitle: string
+  metaTitle: string
+  metaDescription: string
+  firstCategory: TopLevelCategory
+  advantages: ITopPageAdvantage[]
+  createdAt: Date
+  updatedAt: Date
+  hh: IHhData
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import { GetStaticProps } from 'next'
 import { useState } from 'react'
 import { Htag, Button, Ptag, Tag, Rating } from '../components/index'
 import { withLayout } from '../layout/Layout'
+import axios from 'axios'
+import { IMenuItem } from '../interfaces/menu.interface'
 
-// next lesson => 11 - 001 SSR
-
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
   const [rating, setRating] = useState(4)
 
   return (
@@ -35,8 +36,35 @@ function Home(): JSX.Element {
       <Tag size="m" color="primary">
         Size M color primary
       </Tag>
+      <ul>
+        {menu.map((item) => (
+          <li key={item._id.secondCategory}>{item._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   )
 }
 
 export default withLayout(Home)
+
+// next lesson => 12 - 01
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+
+  const { data: menu } = await axios.post<IMenuItem[]>(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+    {
+      firstCategory,
+    }
+  )
+
+  return {
+    props: { menu, firstCategory },
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: IMenuItem[]
+  firstCategory: number
+}
